9f81e22a04f91fc22ac146d34f7afe04a79c8c47
ZaneoBraino <phazonsource@gmail.com>
2022-02-10 15:00:36 -0800

    updating comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const db = require("./db");
 //set up the express functions/server via a const
 const express = require("express");
-//creating a local port
+//creating a local port or use a port given to us
 const PORT = process.env.PORT || 3000;
 
 //console.log(db);
